1a6e849ab9066e6539b7022b32af6ec73808146a
61020959878 <nicoli.ihc875@gmail.com>
2026-09-11 14:11:32 -0300

    feat: Nível 2 - Casting Explícito

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,12 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        int var = 3;
-        double var2 = var;
-        System.out.println("O valor da variavel double e: " + var2);
+
+        double varDouble = 56.99;
+        int varInt = (int) varDouble;
+
+        System.out.println("Valor double: " + varDouble);
+        System.out.println("Valor Int: " + varInt);
+
     }
 }

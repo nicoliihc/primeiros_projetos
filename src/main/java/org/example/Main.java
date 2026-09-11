a697ac49e6e2c44e513ba51e57e29f47d1ef73f6
61020959878 <nicoli.ihc875@gmail.com>
2026-09-11 14:54:21 -0300

    feat: Questão 5 - Dia da Semana

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,19 +8,43 @@ public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Digite a sua nota (0 a 10): ");
-        double nota = entrada.nextDouble();
+        System.out.println("""
+                1 - Domingo
+                2 - Segunda
+                3 - Terça
+                4 - Quarta
+                5 - Quinta
+                6 - Sexta
+                7 - Sábado""");
 
-        if (nota > 10){
-            System.out.println("Nota não computada. Digite a nota novamente (0 a 10): ");
-        }       if (nota >=9){
-            System.out.println("Excelente");
-        }else if (nota >= 7){
-            System.out.println("Bom");
-        } else if (nota >= 5) {
-            System.out.println("Regular");
-        }else {
-            System.out.println("Insuficiente");
+        System.out.println("Digite o número escolhido: ");
+        int num = entrada.nextInt();
+
+        switch (num){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            case 8:
+                System.out.println("Opção inválida");
+                break;
         }
     }
 }

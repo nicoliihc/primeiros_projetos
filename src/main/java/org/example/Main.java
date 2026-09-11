ff19a0dd05dfbe688c2e43f5a66d419e7f4c16bb
61020959878 <nicoli.ihc875@gmail.com>
2026-09-11 14:50:10 -0300

    feat: Questão 3 - Maior Número

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,20 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite a sua nota: ");
-        double nota = entrada.nextDouble();
 
-        if (nota >= 7){
-            System.out.println("Aprovado");
-        }else {
-            System.out.println("Reprovado");
+        System.out.println("Digite o primeiro número: ");
+        int num1 = entrada.nextInt();
+
+        System.out.println("Digite o segundo múmero: ");
+        int num2 = entrada.nextInt();
+
+        if(num1 > num2){
+            System.out.println("O " + num1 + " é maior que " + num2);
+        }
+        if(num1 < num2){
+            System.out.println("O " + num2 + " é maior que " +num1);
+        }else{
+            System.out.println("Os números são iguais");
         }
     }
 }

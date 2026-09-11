f1ad9a45db8c3d5bbc9e84b7ef0521f5d64f78e3
61020959878 <nicoli.ihc875@gmail.com>
2026-09-11 14:09:29 -0300

    feat: Nível 1 - Casting Implícito

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int var = 3;
+        double var2 = var;
+        System.out.println("O valor da variavel double e: " + var2);
+    }
+}

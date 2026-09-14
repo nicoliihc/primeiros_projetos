8f9ea80a898039246f0494d7cda2152e4103636a
61020959878 <nicoli.ihc875@gmail.com>
2026-09-14 15:43:25 -0300

    feat: Atividade 1 - Controle de Entrada em Evento

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,16 +7,12 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Qual é o valor do saldo médio mensal do cliente? ");
-        double saldo = entrada.nextDouble();
 
-        if (saldo >= 5000){
-            System.out.println("O Cliente possui direito à isenção de tarifa bancária");
-        }else{
-            System.out.println("O Cliente NÃO possui direito à isenção de tarifa bancária. A tarifa será mantida");
+        for (int contador = 1; contador <= 10; contador++){
+            System.out.println("Registre o seu nome: ");
+            String nome = entrada.nextLine();
+            System.out.println(nome + " está registrado como participante " + contador);
         }
-
-
 
     }
 }

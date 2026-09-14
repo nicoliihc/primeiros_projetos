7e2709c2bef7f92b6e4df48e0a59faa7016837ef
61020959878 <nicoli.ihc875@gmail.com>
2026-09-14 16:11:00 -0300

    feat: Atividade 3 - Soma de Vendas do Dia

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,16 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
+        Scanner entrada = new Scanner(System.in);
 
-        for (int peca = 1; peca <= 20; peca++){
-            System.out.println("Peça " + peca + " produzida");
+        double venda, soma = 0;
+
+        for (int contador = 1; contador <= 5; contador++){
+            System.out.println("Informe o valor da venda: ");
+            venda = entrada.nextDouble();
+            soma += venda;
         }
+        System.out.println("O total é de R$ " + soma);
 
     }
 }
